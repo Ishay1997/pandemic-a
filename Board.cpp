883b5c1be6a18421ea56478b2e394a0c015ddb91
Ishay1997 <74185897+Ishay1997@users.noreply.github.com>
2021-05-05 18:51:57 +0300

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -4,7 +4,6 @@
 	  Board::Board(){};
 	 int& Board::operator[](const City city){return this->level_disease[city];}
 	 std::ostream& operator<<(std::ostream& out, const Board& board){return out;}
-	 // bool Board::is_clean(){return true;}
 	 void Board::remove_cures(){}
   }
 
